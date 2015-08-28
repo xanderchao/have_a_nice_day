73289463cd9c2663eb6aef3dae83541e61c622b3
Ernesto Malave <emjrdev@gmail.com>
2015-08-28 18:18:17 -0400

    add /surveys post route

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,1 +1,13 @@
-survey.rb
+get '/surveys' do
+  @surveys = Survey.all
+  erb :'/surveys/index'
+end
+
+post '/surveys' do
+  survey = Survey.new(creator_id: session[:user_id], title: params[:title])
+  if survey.save
+    redirect '/surveys/#{survey.id}'
+  else
+    redirect '/surveys'
+  end
+end
