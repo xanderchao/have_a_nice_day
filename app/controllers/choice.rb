220b5e23f6f0ff9cdab2d893e7e1c59ea406695f
Navraj Nat & Alexander Huang <pair+nnat425.xanderhuang@devbootcamp.com>
2015-08-29 16:49:14 -0400

    Add choices to suvey question

--- app/controllers/choice.rb
+++ app/controllers/choice.rb
@@ -1,13 +1,12 @@
+post '/questions/:id/choices' do
+  @question = Question.find_by(id: params[:id])
+  @question.choices.create(text: params[:text])
+  @survey = @question.survey_id
+  redirect "/surveys/#{@survey}"
+end
+
 get "/questions/:id" do
   @question = Question.find_by(id: params[:id])
   @choices = @question.choices
   erb :'/questions/show'
 end
-
-post '/questions/:id/choices' do
-  @question = Question.find_by(id: params[:id])
-  @question.choices.create(text: params[:text])
-  # redirect "/questions/#{@question.id}"
-  redirect ""
-end
-
