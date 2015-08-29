c567367fa8d0cf23d85baf4bed7688d7289dcb62
Ernesto Malave <emjrdev@gmail.com>
2015-08-29 11:28:26 -0400

    add survey/:id route

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -3,10 +3,15 @@ get '/surveys' do
   erb :'/surveys/index'
 end
 
+get '/surveys/:id' do
+  @survey = Survey.find_by(id: params[:id])
+  erb :'/surveys/show'
+end
+
 post '/surveys' do
-  survey = Survey.new(creator_id: session[:user_id], title: params[:title])
-  if survey.save
-    redirect '/surveys/#{survey.id}'
+  @survey = Survey.new(creator_id: session[:user_id], title: params[:title])
+  if @survey.save
+    redirect '/surveys/#{@survey.id}'
   else
     redirect '/surveys'
   end
