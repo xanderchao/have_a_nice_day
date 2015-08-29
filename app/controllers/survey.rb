cd7e6be47fc1dc40b893c915a613bf0b97fabe95
Ernesto Malave <emjrdev@gmail.com>
2015-08-29 11:51:33 -0400

    add questions post route

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -5,13 +5,20 @@ end
 
 get '/surveys/:id' do
   @survey = Survey.find_by(id: params[:id])
+  @questions = @survey.questions
   erb :'/surveys/show'
+end
+
+post '/surveys/:id/questions' do
+  @survey = Survey.find_by(id: params[:id])
+  @survey.questions.create(text: params[:text])
+  redirect '/surveys/:id'
 end
 
 post '/surveys' do
   @survey = Survey.new(creator_id: session[:user_id], title: params[:title])
   if @survey.save
-    redirect '/surveys/#{@survey.id}'
+    redirect '/surveys'
   else
     redirect '/surveys'
   end
