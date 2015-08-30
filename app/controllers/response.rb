c3b157f4ddcf89d16cf956ad56d588d3009f26a0
Alexander <ahuang62wp7c@gmail.com>
2015-08-30 11:17:00 -0400

    get new question to display

--- app/controllers/response.rb
+++ app/controllers/response.rb
@@ -1,5 +1,6 @@
 post "/rounds/:id/responses" do
   @choice = Choice.find_by(id: params[:choice])
   @choice.responses.create(round_id: params[:id])
+  # redirect "/surveys"
   redirect "/rounds/#{params[:id]}"
 end
