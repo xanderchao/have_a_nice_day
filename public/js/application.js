3fa072f44f983dc24ed2b9893dea24a7a33f00f4
Navraj Nat <nnat425@gmail.com>
2015-08-30 00:10:04 -0400

    Question ajax works properly

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,23 @@
 $(document).ready(function () { //When DOM loads this executes
-  $('form', '#create-question').on('submit', function(event){ //Grabs a form with an id create-question
+  // $('form', '#create-question').on('submit', function(event){ //Grabs a form with an id create-question
+  //   event.preventDefault(); // Prevents acction
+  //   $.ajax({ //Lines 4-7 is ajax request to server
+  //     url: $(this).attr('action'),
+  //     method: $(this).attr('method'),
+  //     data: $(this).serialize()
+  //     }).done(function(partial){
+  //       $('#question-list').append('<h3>'+ partial +'</h3>')  ;
+  //   });
+  // }),
+
+$('form', '#create-question').on('submit', function(event){ //Grabs a form with an id create-question
     event.preventDefault(); // Prevents acction
     $.ajax({ //Lines 4-7 is ajax request to server
       url: $(this).attr('action'),
       method: $(this).attr('method'),
       data: $(this).serialize()
       }).done(function(partial){
-        $('#question-list').append('<h3>'+ partial +'</h3>')  ;
+        $('#question-list').html(partial);
     });
   });
-
 });
